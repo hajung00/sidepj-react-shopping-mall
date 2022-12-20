f0e7baee4f8c1537838d5ee4f32df9c8262313c8
hajung00 <gkwjd719@naver.com>
2022-12-20 22:47:14 +0900

    Add: QnA ask Modal

--- src/DetailPage/DetailComponentList/Qna/Qna.js
+++ src/DetailPage/DetailComponentList/Qna/Qna.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
+import { Button, Modal } from 'antd';
 
 function Qna() {
   const qnaDummy = [
@@ -17,6 +18,36 @@ function Qna() {
     },
   ];
   const qnaData = qnaDummy.reverse();
+
+  const [dummy, setDummy] = useState(qnaData);
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [inquiryType, setInquiryType] = useState('default');
+  const [askContent, setAskContent] = useState('');
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen(false);
+    }, 3000);
+    setDummy([
+      ...dummy,
+      { id: 'rj', date: '2022.12.10', state: true, content: askContent },
+    ]);
+    setInquiryType('default');
+    setAskContent('');
+  };
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    console.log('dummy', dummy);
+  }, [dummy]);
+
   return (
     <div style={{ margin: '50px 10px' }}>
       <div style={{ margin: '50px 0px' }}>
@@ -32,7 +63,7 @@ function Qna() {
           </tr>
         </thead>
         <tbody>
-          {qnaData.map((qna) => (
+          {dummy.map((qna) => (
             <tr>
               <td>{qna.state ? '답변 완료' : '미답변'}</td>
               <td>{qna.content}</td>
@@ -54,9 +85,51 @@ function Qna() {
             borderColor: 'gray',
             border: 'none',
           }}
+          onClick={showModal}
         >
           작성하기
         </button>
+        <>
+          <Modal
+            open={open}
+            title='문의하기'
+            onOk={handleOk}
+            onCancel={handleCancel}
+            footer={[
+              <Button key='back' onClick={handleCancel}>
+                취소하기
+              </Button>,
+              <Button
+                key='submit'
+                type='primary'
+                loading={loading}
+                onClick={handleOk}
+              >
+                문의하기
+              </Button>,
+            ]}
+          >
+            <div>
+              <select
+                value={inquiryType}
+                onChange={(e) => {
+                  setInquiryType(e.target.value);
+                }}
+              >
+                <option value='default'> 선택해주세요 </option>
+                <option value='productAsk'> 상품 문의 </option>
+                <option value='shippingAsk'> 배송 문의 </option>
+                <option value='sizeAsk'> 사이즈 문의 </option>
+              </select>
+              <div>
+                <textarea
+                  value={askContent}
+                  onChange={(e) => setAskContent(e.target.value)}
+                ></textarea>
+              </div>
+            </div>
+          </Modal>
+        </>
       </div>
     </div>
   );
